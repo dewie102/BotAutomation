7771e69cbae70b02c4c4ca1b03be58f3dda54d9e
Zack Romasz <zromasz@mail.com>
2023-05-04 07:43:32 -0400

    Trying to combine everything...

---
+++ BotAutomation/BotAutomationMain.cs
@@ -0,0 +1,42 @@
+﻿using System.Data.SqlClient;
+
+namespace BotAutomation
+{
+    internal class BotAutomationMain
+    {
+        static void Main(string[] args)
+        {
+            List<Notice> notices = new();
+
+            string connectionString = "Server=(localdb)\\mssqllocaldb;Database=BotAutomation_Website.Data;Trusted_Connection=True;MultipleActiveResultSets=true";
+            SqlDataReader dataReader;
+            string sql = "SELECT * FROM Notice WHERE Sent = 'False'";
+            SqlCommand command;
+            SqlConnection connection = new SqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                Console.WriteLine("Connection Open!");
+
+                command = new SqlCommand(sql, connection);
+                dataReader = command.ExecuteReader();
+                while(dataReader.Read())
+                {
+                    Notice notice = new((int)dataReader["id"], dataReader["subject"].ToString(), dataReader["Message"].ToString(), dataReader["itemPath"].ToString(),
+                                        (DateTime)dataReader["ScheduledTime"], (bool)dataReader["Sent"], (DateTime)dataReader["LastUpdated"]);
+
+                    notices.Add(notice);
+
+
+                }
+                dataReader.Close();
+                command.Dispose();
+                connection.Close();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Can not open connection!\n{ex.Message}");
+            }
+        }
+    }
+}
---
+++ BotAutomation/Notice.cs
@@ -0,0 +1,30 @@
+﻿namespace BotAutomation
+{
+    public class Notice
+    {
+        public int Id { get; set; }
+
+        public string? Subject { get; set; }
+
+        public string? Message { get; set; }
+
+        public string? ItemPath { get; set; }
+
+        public DateTime ScheduledTime { get; set; }
+
+        public bool Sent { get; set; }
+
+        public DateTime LastUpdated { get; set; }
+
+        public Notice(int id, string? subject, string? message, string? itemPath, DateTime scheduledTime, bool sent, DateTime lastUpdated)
+        {
+            Id = id;
+            Subject = subject;
+            Message = message;
+            ItemPath = itemPath;
+            ScheduledTime = scheduledTime;
+            Sent = sent;
+            LastUpdated = lastUpdated;
+        }
+    }
+}
