085e19482478345f23c33539f3a8f57bfa6623d6
Zack Romasz <zackariah.romasz@yahoo.com>
2024-07-20 19:52:06 -0400

    Upgrade all projects to .net8 Add IDataAccess interface Add SqliteDB class

---
+++ BotAutomationService/IDataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AutomationUtilities.Models;
+
+namespace BotAutomationService
+{
+    internal interface IDataAccess
+    {
+        public Notice GetNotice();
+        public void SaveNotice();
+    }
+}
---
+++ BotAutomationService/SqliteDB.cs
@@ -0,0 +1,35 @@
+﻿using Serilog;
+
+using AutomationUtilities.Models;
+using Microsoft.Data.Sqlite;
+
+namespace BotAutomationService
+{
+    internal class SqliteDB : IDataAccess
+    {
+        SqliteConnection Connection { get; set; }
+
+        public SqliteDB(string databaseFilePath, bool createIfNotExist = true) 
+        {
+            if(!Path.Exists(databaseFilePath) && !createIfNotExist)
+            {
+                Log.Fatal($"Datbase file does not exist: {databaseFilePath}");
+                throw new FileNotFoundException($"Datbase file does not exist: {databaseFilePath}");
+            }
+
+            Connection = new SqliteConnection($"Data Source={databaseFilePath}");
+        }
+
+        public Notice GetNotice()
+        {
+            Notice test = new Notice();
+
+            return test;
+        }
+
+        public void SaveNotice()
+        {
+
+        }
+    }
+}
